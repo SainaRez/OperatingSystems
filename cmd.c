fcb66800ee312227e09193467451fdd1735635d0
JoshDesmond2 <60165551+JoshDesmond2@users.noreply.github.com>
2020-01-24 15:00:51 -0500

    Refactor to new method, switch to execvp

--- cmd.c
+++ cmd.c
@@ -9,9 +9,51 @@
 
 #define BUFSIZE 32
 
+void execute_command(const char *file, char *const argv[]) {
+	int err;
+
+	// Measure start of time
+	struct timeval start_timeval;
+	gettimeofday(&start_timeval, NULL);
+
+	// Measure pagefaults at start:
+	struct rusage rusage_start;
+	getrusage(RUSAGE_CHILDREN, &rusage_start);
+
+	if (fork() == 0) {
+		// child process
+		if (execvp(file, argv) == -1) { // third arg is char *const envp[]
+			err = errno;
+			// TODO/SUGGESTION add args to logging
+			printf("execvp %s failed with %s\n", file, strerror(err));
+			exit(errno);
+		} 
+	} else { // parent process
+		wait(NULL);
+
+		// Measure pagefaults:
+		struct rusage rusage_end;
+		getrusage(RUSAGE_CHILDREN, &rusage_end);
+		long major_faults = rusage_end.ru_majflt - rusage_start.ru_majflt;
+		long minor_faults = rusage_end.ru_minflt - rusage_start.ru_minflt;
+
+		// measure endtime
+		struct timeval end_timeval;
+		gettimeofday(&end_timeval, NULL);
+
+		// diff time
+		struct timeval delta_timeval;
+		timersub(&end_timeval, &start_timeval, &delta_timeval);
+
+		// print statistics:
+		printf("Time elapsed: %ld.%06ld seconds\n", delta_timeval.tv_sec, delta_timeval.tv_usec); // TODO change to milliseconds
+		printf("Page Faults: %ld\n", major_faults);
+		printf("Page Faults (reclaimed): %ld\n", minor_faults);
+	}
+}
+
 int main() {
 	char cmd[BUFSIZE];
-	int err;
 
 	while(1) {
 		// Input next command from fgets
@@ -20,47 +62,13 @@ int main() {
 			exit(0);
 		}
 
+		// TODO, use the strtok function to parse the string in an array of char* argv[]. (as per hw guidelines)
+
 		// "Moving where the null terminator is by one to avoid the new line problem"
 		cmd[strcspn(cmd, "\n")] = 0;
 		printf("Received: %s\n", cmd); // TEMP logging
 
-		// Measure start of time
-		struct timeval start_timeval;
-		gettimeofday(&start_timeval, NULL);
-
-		// Measure pagefaults at start:
-		struct rusage rusage_start;
-		getrusage(RUSAGE_CHILDREN, &rusage_start);
-	
-		if (fork() == 0) {
-			// child process
-			if (execl(cmd, cmd, NULL) == -1) {
-				err = errno;
-				printf("exec %s failed with %s\n", cmd, strerror(err));
-				exit(errno);
-			} 
-		} else { // parent process
-			wait(NULL);
-
-			// Measure pagefaults:
-			struct rusage rusage_end;
-			getrusage(RUSAGE_CHILDREN, &rusage_end);
-			long major_faults = rusage_end.ru_majflt - rusage_start.ru_majflt;
-			long minor_faults = rusage_end.ru_minflt - rusage_start.ru_minflt;
-
-			// measure endtime
-			struct timeval end_timeval;
-			gettimeofday(&end_timeval, NULL);
-
-			// diff time
-			struct timeval delta_timeval;
-			timersub(&end_timeval, &start_timeval, &delta_timeval);
-
-			// print statistics:
-			printf("Time elapsed: %ld.%06ld seconds\n", delta_timeval.tv_sec, delta_timeval.tv_usec); // TODO change to milliseconds
-			printf("Page Faults: %ld\n", major_faults);
-			printf("Page Faults (reclaimed): %ld\n", minor_faults);
-		}	
+		execute_command(cmd, NULL); // TODO, null should be argv[]
 	}
 }
 
