04eaf084aed3b32d6ae559c51d830bd218075a3b
JoshDesmond2 <60165551+JoshDesmond2@users.noreply.github.com>
2020-01-31 21:39:42 -0500

    multi commands are generally functional

--- ThreeCommander/multi.c
+++ ThreeCommander/multi.c
@@ -6,6 +6,7 @@
 #include <sys/wait.h>
 #include <sys/time.h>
 #include <sys/resource.h>
+#include <stdbool.h>
 
 
 /**
@@ -37,9 +38,8 @@ void execute_command(int argc, char *const argv[]) {
 	int pid = fork();
 	if (pid == 0) {
 		// child process
-		if (execvp(argv[0], argv) == -1) { // third arg is char *const envp[]
+		if (execvp(argv[0], argv) == -1) {
 			err = errno;
-			// TODO/SUGGESTION add args to logging
 			printf("execvp %s failed with %s\n", argv[0], strerror(err));
 			exit(errno);
 		} 
@@ -74,6 +74,66 @@ void execute_command(int argc, char *const argv[]) {
 		printf("Fork failed");
 	}
 }
+/**
+ * Executes a command using execvp, and measures the time of the 
+ * execution of the command, and the pagefaults of the execution of the command
+ *
+ * @param int argc
+ * 	The number of elements in the array argv
+ * @param char *const argv[]
+ * 	The args. Assumes argv[0] is == the file name
+ * @param int background_id
+ * 	The project-specific "background ID" of the argument.
+ */
+void execute_multi_command(int argc, char *const argv[], int background_id) {
+	// Prints the 'running command' log
+	printf("Running background command: ");
+	int i; 
+	for(i = 0; i < argc; i++)
+		printf("%s ", argv[i]);
+	printf("\n");
+	// TODO print "Background: ID [%i]: <command>\n\n", background_id
+
+	int err;
+	int pid = fork();
+	if (pid == 0) { // Child process
+
+		int pid2 = fork();
+
+		// Measure start of time
+		struct timeval start_timeval;
+		gettimeofday(&start_timeval, NULL);
+
+		if (pid2 == 0) { // Grandchild process
+			// run process
+			if (execvp(argv[0], argv) == -1) {
+				err = errno;
+				printf("execvp %s failed with %s\n", argv[0], strerror(err));
+				exit(errno);
+			} 
+		} else if (pid2 > 0) { // Not the Grandchild process
+			wait(NULL); // TODO maybe use wait0
+			// measure endtime
+			struct timeval end_timeval;
+			gettimeofday(&end_timeval, NULL);
+
+			// diff time
+			struct timeval delta_timeval;
+			timersub(&end_timeval, &start_timeval, &delta_timeval);
+
+			// print time
+			printf("Time elapsed: %ld.%06ld seconds\n", delta_timeval.tv_sec, delta_timeval.tv_usec); 
+			exit(0);
+		}
+	} else if (pid > 0) { // parent process
+		// TODO
+		printf("Returning from fork. There is now a child running with PID %i\n", 42);
+		return;
+	}
+	else {
+		printf("Fork failed");
+	}
+}
 
 void execute_boring_commander() {
 	char* argv1[] = {"whoami", NULL};
@@ -84,14 +144,37 @@ void execute_boring_commander() {
 	execute_command(3, argv3);
 };
 
+bool wait_for_process() {
+	int status = 0;
+	struct rusage usage;
+	int wait_pid = wait3(&status, WNOHANG, &usage);
+	if (wait_pid == 0) { return true; }
+	else if (wait_pid > 0) {
+		printf("PID %i returned!\n", wait_pid);
+	}
+
+	return false;
+}
+
+typedef struct {
+	char command[128];
+	int background_id;
+} background_command;
+
+typedef struct node {
+	struct background_command;
+	struct node * next;
+} background_list;
+
 void process_text_file(const char *filename) {
 	FILE *file; 
 	file = fopen(filename, "r");
 	char cmd[128];
 	int file_line_number = 1; // 1 because linenumbers start at 1
+	int background_id_counter = 0;
 	while(fgets(cmd, 128, file) != NULL) {
 		cmd[strcspn(cmd, "\n")] = 0; // Remove newline suffix from command string
-		printf("Prcessing CMD: %s, line %i\n", cmd, file_line_number);
+		printf("Processing CMD: %s, line %i\n", cmd, file_line_number);
 		char* token = strtok(cmd, " ");
 		char* argv[32]; 
 		int arg_counter = 0;
@@ -99,11 +182,27 @@ void process_text_file(const char *filename) {
 		while(token != NULL) {
 			argv[arg_counter] = token;
 			token = strtok(NULL, " ");
- 			arg_counter++;
+			arg_counter++;
 		}
-		
+		argv[arg_counter] = NULL; // Make sure our argv is null terminated
+
+		// Branch based on multi_thread_line_numbers
+		if (file_line_number == 2) { // TODO temp
+			execute_multi_command(arg_counter, argv, background_id_counter++);
+
+			// keep track of background commands in a struct
+		} else {
+			execute_command(arg_counter, argv);
+		}
+
+		wait_for_process();
 		file_line_number++;
- 	}
+	}
+
+	// Now just wait for all multi-threaded processes to finish
+	while(wait_for_process()) {
+		// pass
+	}
 }
 
 void parse_line_number_arguments() {
@@ -111,8 +210,8 @@ void parse_line_number_arguments() {
 }
 
 int main(int argc, char *argv[]) {
-	execute_boring_commander();
-	
+	// execute_boring_commander();
+
 	// TODO
 	int multi_threaded_line_numbers[2] = {1,3};
 	parse_line_number_arguments();
