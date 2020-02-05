4719155634410859a2c0b1f1db28309ce08d4f5b
JoshDesmond2 <60165551+JoshDesmond2@users.noreply.github.com>
2020-02-04 20:09:04 -0500

    Added new stub function

--- VirtualMemory/main.c
+++ VirtualMemory/main.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 
+#define SIZE 128
+
+/**
+ * TODO
+ *
+ * @param
+ * @param char instruction_type[]
+ * 	one of {m, s, l}, where 
+ * 	- m represents map,
+ * 	- s represents store, and
+ * 	- l represents load
+ * @param
+ * @param 
+ */
+void process_commmand(int process_id, char instruction_type, int virtual_address, int value) {
+
+}
+
 int main() {
 	printf("Hello world!");
 }
