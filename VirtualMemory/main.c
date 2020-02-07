cc551697dcd135a356333ed21e59c9d738a31b50
JoshDesmond <JoshDesmond@users.noreply.github.com>
2020-02-07 02:47:29 -0500

    Autoformatting

--- VirtualMemory/main.c
+++ VirtualMemory/main.c
@@ -21,9 +21,9 @@ int page_table_register_array[MAX_PROCESSES];
  * Initialize all page_table_registers to -1
  */
 void initialize_register_array() {
-	for (int i = 0; i < 4; i++) {
-		page_table_register_array[i] = -1;
-	}
+    for (int i = 0; i < 4; i++) {
+        page_table_register_array[i] = -1;
+    }
 }
 
 
@@ -31,14 +31,14 @@ void initialize_register_array() {
  * Utility function to print the entire contents of memory
  */
 void print_memory() {
-	for (int y = 0; y < SIZE/16; y++) {
-		for (int x = 0; x < 16; x++) {
-			printf("0x%04x, ", memory[16*y + x]);
-		}
-		printf("\n");
-	}
+    for (int y = 0; y < SIZE / 16; y++) {
+        for (int x = 0; x < 16; x++) {
+            printf("0x%04x, ", memory[16 * y + x]);
+        }
+        printf("\n");
+    }
 
-	printf("\n");
+    printf("\n");
 }
 
 
@@ -46,7 +46,7 @@ void print_memory() {
  * Returns true if the given process has a page table initialized into memory yet
  */
 bool does_process_have_page_file(int process_id) {
-	return (page_table_register_array[process_id] == -1);
+    return (page_table_register_array[process_id] == -1);
 }
 
 /**
@@ -64,7 +64,6 @@ void store(int process_id, int virtual_address, int value) {
 }
 
 
-
 /**
  * TODO
  *
@@ -75,8 +74,8 @@ void store(int process_id, int virtual_address, int value) {
  * @param int value
  */
 void map(int process_id, int virtual_address, int value) {
-	// TODO this method can include logic for parameter validation (since
-	// much of it is the same accross all the commands
+    // TODO this method can include logic for parameter validation (since
+    // much of it is the same accross all the commands
 }
 
 
@@ -93,12 +92,12 @@ void map(int process_id, int virtual_address, int value) {
  * @param 
  */
 void process_commmand(int process_id, char instruction_type, int virtual_address, int value) {
-	
-	if(3 < process_id && process_id < 0){
-		printf("process ID is out of range");
-	}
 
-	
+    if (3 < process_id && process_id < 0) {
+        printf("process ID is out of range");
+    }
+
+
 }
 
 
@@ -106,10 +105,10 @@ void process_commmand(int process_id, char instruction_type, int virtual_address
  * Loops the read, evaluate, and print loop
  */
 void loop_repl() {
-	int pid, vir_addrs, value;
+    int pid, vir_addrs, value;
     char command[512], exit_check[512];
-    
-    while(1){
+
+    while (1) {
 
         printf("Instruction?  ");
         scanf("%i,%s,%i,%i\n", &pid, command, &vir_addrs, &value);
@@ -117,21 +116,21 @@ void loop_repl() {
         printf("Are you done? (type yes or no) \n");
         scanf("%s", exit_check);
 
-        if (strcmp(exit_check, "yes") == 0){
+        if (strcmp(exit_check, "yes") == 0) {
             exit(1);
         }
     }
 }
 
 int main() {
-	initialize_register_array();
-	print_memory();
+    initialize_register_array();
+    print_memory();
 
-	unsigned char foo = 65;
-	unsigned char bar = 66;
-	memory[16] = foo;
-	memory[17] = bar;
+    unsigned char foo = 65;
+    unsigned char bar = 66;
+    memory[16] = foo;
+    memory[17] = bar;
 
-	print_memory();
+    print_memory();
 
 }
