a38f7c359080347f7d8587746e529b279a278f9b
JoshDesmond <JoshDesmond@users.noreply.github.com>
2020-02-09 22:27:19 -0500

    Fixed and updated testing script

--- VirtualMemory/Test.sh
+++ VirtualMemory/Test.sh
@@ -1,2 +1,12 @@
 gcc -o main.o main.c
-./main.o < ./test_commands.txt
+
+PROGRAM="main.o"
+
+TEST_FILE="./test_simple.txt"
+echo "Running $PROGRAM with $TEST_FILE"
+./main.o < $TEST_FILE
+
+TEST_FILE="./test_commands.txt"
+echo -e "\n\n"
+echo "Running $PROGRAM with $TEST_FILE"
+./main.o < $TEST_FILE
