8749ab232430c38fb89c536d6d4992f3d96875b3
Saina Rezvani <saina.r6@gmail.com>
2020-02-13 03:12:10 -0500

    Completed map for the most part

--- VirtualMemory/memory_manager.c
+++ VirtualMemory/memory_manager.c
@@ -20,11 +20,13 @@ int page_table_base_register[PTBR_SIZE];
 int available_memory[FREE_MEMORY_SIZE];
 
 typedef struct Entry {
-    unsigned char protectionBits;
-    //unsigned char is_used;
+    
     unsigned char virtual_page;
     unsigned char physical_page;
+    unsigned char protectionBits;
     unsigned char present;
+    unsigned char free;
+
 } Entry;
 
 typedef struct Page_Table {
@@ -44,11 +46,10 @@ void initialize_arrays(){
 
 //checks if a memory is available and return the first index of the free memory. If none is available,
 //it returns -1
-int free_memory_index(int pid) {
+int free_memory_index() {
     for (int i = 0; i < FREE_MEMORY_SIZE; i++) {
         if (available_memory[i] == 0) {
             available_memory[i] = 1;
-            page_table_base_register[pid] = i * PAGE_SIZE;
             return i;
         }
         else {
@@ -74,11 +75,12 @@ int get_virtual_page(int virtual_address) {
 // Accesses the page table based on the memory address
 // Looks for the entry that has that virtual page number
 
-Entry get_entry_of_virtual_page(int vir_addr, int memory_address) {
-    Page_Table table = memory[memory_address];
+Entry* get_entry_of_virtual_page(int vir_page, int memory_address) {
+    Page_Table *table = (Page_Table*) &memory[memory_address];
     for (int i = 0; i < PAGE_TABLE_ENTRY_NUM; i++) {
-        if (table.entries[i].virtual_page == vir_addr) {
-            return table.entries[i];
+        if (table->entries[i].virtual_page == vir_page) {
+            Entry *entry = &table->entries[i];
+            return entry;
         }
     }
     return NULL;
@@ -87,26 +89,61 @@ Entry get_entry_of_virtual_page(int vir_addr, int memory_address) {
 // Maps the virtual memory to the physical memory
 void map(int pid, int virtual_address, unsigned char value){
     int memory_index;
-    if (page_table_base_register[pid] == -1) {  //it means that process isn't mapped yet
-        memory_index = free_memory_index(pid); //get the index of first free memory
+    if (page_table_base_register[pid] == -1) {  //it means that process isn't mapped at all
+        memory_index = free_memory_index(); //get the index of first free memory
         if (memory_index == -1) {
-            printf("Error: No memory is available\n");
+            printf("Error: No memory is available for the page table\n");
             return;
         }
         else {
-            int virtual_page = get_virtual_page(virtual_address);
-            struct Entry Page_table_entry = {value, virtual_page, memory_index, 1};
+            // Create a page table at the memory address in page_table_base_register
+            page_table_base_register[pid] = memory_index * PAGE_SIZE;
+            printf("Put page table for PID %i into physical frame %i\n", pid, memory_index);
+            
+            // Now we check the memory again for the entry this time (last time was page table)
+            if (free_memory_index() == -1) { //not sure needs to be confirmed
+                printf("No Memory is available for entry\n");
+            }
+            else {
+                int virtual_page = get_virtual_page(virtual_address);
+                Entry page_table_entry = {virtual_page, memory_index, value, 1, 1};
+               
+                Page_Table *new_table;
+                new_table->entries[0] = page_table_entry;
+                
+                int memory_address = page_table_base_register[pid];
+                new_table = (Page_Table *) &memory[memory_address];
+                //(Page_Table *) &memory[memory_address] = &new_table;
+
+            }            
         }
     }
-    else {
-        printf("Warning: Memory already mapped\n");
-        return;
+    // Is it possible to initialize the free variable so we just check that?
+    else { //process is mapped before so we check for the specific virtual page
+        int virtual_page = get_virtual_page(virtual_address);
+        Entry *page_table_entry = get_entry_of_virtual_page(virtual_page, page_table_base_register[pid]);
+        if (page_table_entry == NULL) {
+            Entry page_table_entry = {virtual_page, memory_index, value, 1, 1};
+            //appned it to the entreis
+            printf("Mapped virtual address %i (page %i) into physical frame %i\n", virtual_address, virtual_page,
+            memory_index);
+        }
+        else {
+            printf("Warning: Memory already mapped with the given virtual address\n");
+            return;
+        }
+
     }
+    printf("available_memory:\n");
     for (int i = 0; i < PTBR_SIZE; i++) {
-        printf("index %i in available memory: %i\n", i, available_memory[i]);
-        printf("index %i in page table base register: %d\n", i,
-         page_table_base_register[i]);
+        printf("%i ", available_memory[i]);
     }
+    printf("\n");
+    printf("page_table_base_register:\n");
+    for (int i = 0; i < PTBR_SIZE; i++) {
+        printf("%d ", page_table_base_register[i]);
+    }
+    printf("\n");
 
     return;  
 }
@@ -125,19 +162,19 @@ void store(int pid, int virtual_address, int value){
     }
     else {
         int virtual_page = get_virtual_page(virtual_address);
-        Entry entry = get_entry_of_virtual_page(virtual_address, memory_address);
+        Entry *entry = get_entry_of_virtual_page(virtual_page, memory_address);
         if (entry == NULL) {
             printf("Error: No entry with the given virtual address was found\n");
             return;
         }
         else {
             // Frist check if it has write acess
-            if (entry.protectionBits == 0) {
+            if (entry->protectionBits == 0) {
                 printf("Entry does not privide write acess");
                 return;
             }
             else {
-                int frame = entry.physical_page;
+                int frame = entry->physical_page;
                 int offset = virtual_address % PAGE_SIZE;
                 int physical_address = (frame * PAGE_SIZE) + offset;
                 memory[physical_address] = value;
@@ -160,17 +197,18 @@ void load(int pid, int virtual_address){
     }
     else {
         int virtual_page = get_virtual_page(virtual_address);
-        Entry entry = get_entry_of_virtual_page(virtual_address, memory_address);
+        Entry *entry = get_entry_of_virtual_page(virtual_page, memory_address);
         if (entry == NULL) {
             printf("Error: No entry with the given virtual address was found\n");
             return;
         }
         else {
-            int frame = entry.physical_page;
+            int frame = entry->physical_page;
             int offset = virtual_address % PAGE_SIZE;
             int physical_address = (frame * PAGE_SIZE) + offset;
             printf("The value %i is at virtual address %i\n", memory[physical_address], virtual_address);
         }
+    }
     return;
 }
 
@@ -179,13 +217,16 @@ void process_command(int pid, char *command, int vir_addrs, int value){
 
     if (pid < 0 || pid > 3) {
         printf("Error: PID out of range\n");
+        return;
     }
     if (vir_addrs < 0 || vir_addrs > 63) {
         printf("Error: virtual address out of memory range\n");
+        return;
     }
     if (value < 0 || value > 1) {
         printf("value: %c\n", value);
         printf("Error: protection value not supported\n");
+        return;
     }
 
     if (strcmp(command, "map") == 0) {
@@ -193,10 +234,10 @@ void process_command(int pid, char *command, int vir_addrs, int value){
 
     }
     else if (strcmp(command, "store") == 0) {
-        //store();
+        store(pid, vir_addrs, value);
     }
     else if (strcmp(command, "load") == 0) {
-        //load();
+        load(pid, vir_addrs);
     }
     else {
         printf("Command not supported\n");
@@ -217,6 +258,7 @@ void loop_repl(int argc, char* argv[]){
         int arg_count = scanf("%i,%6[^,^\n],%i,%i", &pid, command, &vir_addrs, &value);
         if (arg_count != 4){
             printf("Wrong number of inputs");
+            return;
         }
         printf("count: %d\n",  arg_count);
 
