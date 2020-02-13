ad6be2506183a5a6dc9fa6f2a030c51dd25c8be0
Saina Rezvani <saina.r6@gmail.com>
2020-02-12 20:29:06 -0500

    Added get_virtual_page and else statement to map

--- VirtualMemory/memory_manager.c
+++ VirtualMemory/memory_manager.c
@@ -11,7 +11,7 @@
 #define MAX_MEMORY_SIZE 64
 #define FREE_MEMORY_SIZE 4
 #define PAGE_TABLE_ENTRY_NUM 4
-#define FRAME_SIZE 16
+#define PAGE_SIZE 16
 
 unsigned char memory[MAX_MEMORY_SIZE];
 
@@ -21,9 +21,10 @@ int available_memory[FREE_MEMORY_SIZE];
 
 typedef struct Entry {
     unsigned char protectionBits;
-    unsigned char is_used;
+    //unsigned char is_used;
     unsigned char virtual_page;
     unsigned char physical_page;
+    unsigned char present;
 } Entry;
 
 typedef struct Page_Table {
@@ -41,65 +42,54 @@ void initialize_arrays(){
     return;
 }
 
-//checks if a memory is available and retunr the first index
+//checks if a memory is available and return the first index of the free memory. If none is available,
+//it returns -1
 int free_memory_index(int pid) {
     for (int i = 0; i < FREE_MEMORY_SIZE; i++) {
         if (available_memory[i] == 0) {
             available_memory[i] = 1;
-            page_table_base_register[pid] = i * FRAME_SIZE;
+            page_table_base_register[pid] = i * PAGE_SIZE;
             return i;
         }
         else {
             if (i == FREE_MEMORY_SIZE - 1) {
-                printf("No frame available");
+                // printf("No frame available");
                 return -1;
             }
         }
     }
 }
 
-// Maps  the virtual memory tothe physical memory
-void map(int pid){
+// Gets a page address between 0 and 63 and returns the page number. If address is in 
+// range[0,15], page is 0. If address in in range [16,31], page is 1. If address is in 
+// range [32,47], page is 2 and if address is in range [48,63], page is 3.
+
+int get_virtual_page(int virtual_address) {
+    int virtual_page;
+    virtual_page = (int)(virtual_address / PAGE_SIZE);
+    return virtual_page;
+}
+
+// Maps the virtual memory to the physical memory
+void map(int pid, int virtual_address, unsigned char value){
     int memory_index;
-    if (page_table_base_register[pid] == -1) {
-        memory_index = free_memory_index(pid);
+    if (page_table_base_register[pid] == -1) {  //it means that process isn't mapped yet
+        memory_index = free_memory_index(pid); //get the index of first free memory
         if (memory_index == -1) {
-            printf("no memory is available");
+            printf("No memory is available");
             return;
         }
         else {
-            int virtual_page = (virtual_address - (virtual_addres % FRAME_SIZE)) / FRAME_SIZE;
-            Entry *page_table_base_register = 
+            int virtual_page = get_virtual_page(virtual_address);
+            struct Entry Page_table_entry = {value, virtual_page, memory_index, 1};
         }
     }
-
-
-
-
+    else {
+        printf("Memory already mapped");
+    }
 
     return;
-    // if (page_table_base_register[pid] == -1) {
-        
-    //     for(int i = 0; i < FREE_MEMORY_SIZE; i++) {
-    //         if (available_memory[i] == 0) {
-    //             available_memory[i] = 1; 
-    //             page_table_base_register[pid] = i;
-    //             exit(1);
-    //         }
-    //         else {
-    //             if(i == FREE_MEMORY_SIZE-1) {
-    //                 printf("No frame available");
-    //                 return;
-    //             }
-    //         }
-    //     }
-        
-    //     //int memory_location = (page_table_base_register[pid] * 16) + page;
-    // }
-    // else {
-    //     printf("Page Table is already created for this PID");
-    //     return;
-    // }
+    
 }
 
 // Instructs the memory manager to write  the supplied value
@@ -115,7 +105,7 @@ void load(){
 
 }
 
-void process_command(const int pid, const char *command, const  int vir_addrs, const int value){
+void process_command(int pid, char *command, int vir_addrs, unsigned char value){
 
     if (pid < 0 && pid > 3){
         printf("PID out of range");
@@ -126,10 +116,10 @@ void process_command(const int pid, const char *command, const  int vir_addrs, c
 
     }
     else if (strcmp(command, "store") == 0) {
-
+        //store();
     }
     else if (strcmp(command, "load") == 0) {
-
+        //load();
     }
     else {
         printf("Command not supported");
@@ -141,14 +131,14 @@ void process_command(const int pid, const char *command, const  int vir_addrs, c
 
 void loop_repl(int argc, char* argv[]){
 
-    int value;
+    unsigned char value;
     int pid, vir_addrs;
     char command[7], exit_check[4];
     
     while(1){
 
         printf("Instruction? ");
-        int arg_count = scanf("%i,%6[^,^\n],%i,%i", &pid, command, &vir_addrs, &value);
+        int arg_count = scanf("%i,%6[^,^\n],%i,%c", &pid, command, &vir_addrs, &value);
         if (arg_count != 4){
             printf("Wrong number of inputs");
         }
